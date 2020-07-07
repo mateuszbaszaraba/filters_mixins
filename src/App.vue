c761be23131fa7388c1baa37eb4994c222c51037
Matt <mateuszbaszaraba@outlook.com>
2020-07-07 14:17:41 +0200

    alternative - computed

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 mt-5">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUppercase | to-lowercase }}</p>
+        <hr>
+        <input v-model="filterText" />
+        <ul>
+          <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -14,12 +19,21 @@ export default {
   name: 'app',
   data () {
     return {
-      text: 'Hola senorita!'
+      text: 'Hola senorita!',
+      fruits: ['apple', 'banana', 'mango', 'melon'],
+      filterText: ''
     }
   },
   filters: {
     toUppercase(value) {
       return value.toUpperCase()
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter(element => {
+        return element.match(this.filterText)
+      })
     }
   }
 }
