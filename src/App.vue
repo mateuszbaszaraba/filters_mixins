0766ac736450baabf65846c98db23dea972ba226
Matt <mateuszbaszaraba@outlook.com>
2020-07-07 08:29:02 +0200

    created local filter

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 mt-5">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +14,12 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      text: 'Hola senorita!'
+    }
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase()
     }
   }
 }
